90347334824170b8bdabe4172105625123cdbb20
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-12 02:24:33 +0600

    Update SVM-on-Diabates-Dataset.py

--- L8-SVM-Basics/SVM-on-Diabates-Dataset.py
+++ L8-SVM-Basics/SVM-on-Diabates-Dataset.py
@@ -1,1 +1,14 @@
+import pandas as pd
+df = pd.read_csv("diabetes.csv")
+df
 
+X = df.drop(["Outcome"], axis =1)
+y= df["Outcome"]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=50)
+
+from sklearn import svm
+clf = svm.SVC()
+clf.fit(X_train, y_train)
